ca6f4be6926be368936a201a5fc556d07bb64be9
tom_glb <goodMorning_glb@atguigu.com>
2019-08-07 10:33:49 +0800

    UpdateCommit

---
+++ src/decorator/starbuzz/CondimentDecorator.java
@@ -0,0 +1,8 @@
+package decorator.starbuzz;
+
+/**
+ * 调料扩展自Beverage
+ */
+public abstract class CondimentDecorator extends Beverage{
+    public abstract String getDescription();
+}
---
+++ src/decorator/starbuzz/Whip.java
@@ -0,0 +1,24 @@
+package decorator.starbuzz;
+
+/**
+ * Created by Gavin on 2017/3/7.
+ */
+public class Whip extends CondimentDecorator {
+    Beverage beverage;
+
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    @Override
+    public double cost() {
+        return 0.10 + beverage.cost();
+    }
+
+
+}
